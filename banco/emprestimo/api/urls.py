adbc73b0c0922dadda7815b23e83a5bf495dd136
Caique Menezes <115851755+caiquedevjs@users.noreply.github.com>
2024-04-19 21:06:46 +0000

    implenetação da api

---
+++ banco/emprestimo/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import SimulacaoEmprestimoCreateView
+
+urlpatterns = [
+    path("criar_emprestimo", SimulacaoEmprestimoCreateView.as_view()),
+]
---
+++ banco/emprestimo/api/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import SimulacaoEmprestimoSerializer
+
+class SimulacaoEmprestimoCreateView(generics.ListCreateAPIView):
+
+    def post(self, request, *args,  **kwargs):
+        serializer = self.get_serializer(data=request.data, context=({'request' : request}))
+
+        if serializer.is_valid():
+            retorno_salvamento = serializer.save()
+
+        return Response(
+            {"data" : SimulacaoEmprestimoSerializer(
+                retorno_salvamento, context= self.get_serializer_context()).data, "result" : "Dados Salvoos com sucesso"},
+                
+                status = status.HTTP_201_CREATED
+        )
