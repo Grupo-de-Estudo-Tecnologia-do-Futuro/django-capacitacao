93947a4e9b76aaa567bd562e83aa005a77e5d2e9
Rafael  Alves <97904425+RafaelAlvesTech@users.noreply.github.com>
2024-04-19 13:43:24 +0000

    views

--- aplicacao_inicio/urls.py
+++ aplicacao_inicio/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from primeiro_app import primeiro_app
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include(primeiro_app.urls))
 ]
