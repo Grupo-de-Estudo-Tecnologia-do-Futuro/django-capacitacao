8cd1a6ad668f37d0786b4f536635627738642d69
Rafael  Alves <97904425+RafaelAlvesTech@users.noreply.github.com>
2024-04-21 15:40:27 +0000

    add

--- banco/emprestimo/api/serializers.py
+++ banco/emprestimo/api/serializers.py
@@ -1,30 +1,33 @@
 from rest_framework import serializers
-from emprestimo.models import SimulacaoEmprestimo
+from emprestimo.models import SimulacaoEmprestimo # model criado na pasta models.py
 
 class SimulacaoEmprestimoSerializer(serializers.ModelSerializer):
     id = serializers.CharField()
-    valor_emprestimo = serializers.DecimalField(max_digits=10, decimal_places=2)
-    taxa_juros = serializers.DecimalField(max_digits=5, decimal_places=2)
-    prazo_meses= serializers.IntegerField()
+    valor_emprestimo = serializers.DecimalField(max_digits =10, decimal_places=2)
+    taxa_juros = serializers.DecimalField(max_digits =5, decimal_places=2)
+    prazo_meses = serializers.IntegerField()
 
-    class Meta:
-        model = SimulacaoEmprestimo
-        fields = ['id', 'valor_emprestimo', 'taxa_juros', 'prazo_meses']
+class Meta:
+    model = SimulacaoEmprestimo
+    fields = ['id', 'valor_emprestimo','taxa_juros', 'prazo_meses']
 
-        def save (self, **kwargs):
-            data_send = self.initial_data #dado vque vem da requisição
+    def save(self,**kwargs):
+        data_send = self.initial_data #dado que vem da requisição
 
-            # Obter dados oriundos do JSON do self.initial_data recebidos da View(data_send)
-            id = data_send['id']
-            valor_emprestimo = data_send['valor_emprestimo']
-            taxa_juros = data_send['taxa_juros']
-            prazo_meses = data_send['prazo_meses']
 
-            simulacaoEmpObj = SimulacaoEmprestimo( id = id,
-                valor_emprestimo = valor_emprestimo,
-                taxa_juros = taxa_juros,
-                prazo_meses = prazo_meses,
-            )
-            simulacaoEmpObj.save()
+        # obter dados oriundos JSON do self.initial_data recebidos da View(data_send)
+        id = data_send['id']
+        valor_emprestimo = data_send['valor_emprestimo']
+        taxa_juros = data_send['taxa_juros']
+        prazo_meses = data_send['prazo_meses']
 
-            return simulacaoEmpObj
+        SimulacaoEmpObj = SimulacaoEmprestimo(
+            id = id,
+            valor_emprestimo = valor_emprestimo,
+            taxa_juros = taxa_juros,
+            prazo_meses = prazo_meses,
+        )
+
+        # salvando no banco de dados
+        SimulacaoEmpObj.save()
+        return SimulacaoEmpObj
--- banco/emprestimo/api/views.py
+++ banco/emprestimo/api/views.py
@@ -2,19 +2,17 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from .serializers import SimulacaoEmprestimoSerializer
 
-
 class SimulacaoEmprestimoCreateView(generics.ListCreateAPIView):
-    serializer_class = SimulacaoEmprestimoSerializer
 
-    def post (self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data, context=({'request':request}))
+    def post(self, request, *args,  **kwargs):
+        serializer = self.get_serializer(data=request.data, context=({'request' : request}))
 
         if serializer.is_valid():
-            retorno_salvadmento = serializer.save()
+            retorno_salvamento = serializer.save()
 
-            return Response(
-                {"data": SimulacaoEmprestimoSerializer(retorno_salvadmento,
-                                                      context=self.get_serializer_context()).data,
-                "result": "Dados salvos"
+        return Response(
+            {"data" : SimulacaoEmprestimoSerializer(
+                retorno_salvamento, context= self.get_serializer_context()).data, "result" : "Dados Salvoos com sucesso"},
                 
-            }, status = status.HTTP_201_CREATED)
+                status = status.HTTP_201_CREATED
+        )
