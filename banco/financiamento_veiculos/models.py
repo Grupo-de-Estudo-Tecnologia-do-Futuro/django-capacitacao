22848a3c4318e4a9e328d74ea66542d19a1c162a
Caique Menezes <115851755+caiquedevjs@users.noreply.github.com>
2024-04-24 14:27:27 +0000

    fazendo a as migrações para o banco

--- banco/financiamento_veiculos/models.py
+++ banco/financiamento_veiculos/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class SimulacaoEmprestimo(models.Model):
+class FinanciamentoVeiuclos(models.Model):
     id= models.CharField(primary_key=True, max_length=10)
-    valor_veiculos = models.FloatField(max_digits=2, decimal_places=2)
-    entrada_veiculos = models.FloatField(max_digits=2, decimal_places= 2)
+    valor_veiculos = models.DecimalField(max_digits=5, decimal_places=2)
+    entrada_veiculos = models.DecimalField(max_digits=2, decimal_places= 2)
     prazo_financiamento = models.IntegerField()
