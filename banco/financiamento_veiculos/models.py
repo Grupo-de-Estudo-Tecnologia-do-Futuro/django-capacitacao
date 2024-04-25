0fbbb5f73b445e907967af2b98a3e14c3a5386f0
Caique Menezes <115851755+caiquedevjs@users.noreply.github.com>
2024-04-25 14:37:42 +0000

    todas as rotas testadas

--- banco/financiamento_veiculos/models.py
+++ banco/financiamento_veiculos/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class FinanciamentoVeiculos(models.Model):
     id= models.CharField(primary_key=True, max_length=10)
-    valor_veiculos = models.DecimalField(max_digits=5, decimal_places=2)
-    entrada_veiculos = models.DecimalField(max_digits=2, decimal_places= 2)
+    valor_veiculos = models.DecimalField(max_digits=10, decimal_places=2)
+    entrada_veiculos = models.DecimalField(max_digits=5, decimal_places= 2)
     prazo_financiamento = models.IntegerField()
